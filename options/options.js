4bccd04ffdc9d63c6ef95d01b1784b9d115aa441
Ravinder sales <ravinder.y@crowdbotics.com>
2023-04-18 15:36:42 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "connector-ravinder--77273",
-  url: "https://connector-ravinder--77273.botics.co",
-  api: "https://connector-ravinder--77273.botics.co/api/v1"
-}
+  "name": "connector-ravinder--77273",
+  "url": "https://connector-ravinder--77273.botics.co",
+  "api": "https://connector-ravinder--77273.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
