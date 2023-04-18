8101179224e64f4cc5475e29dc22a408e64bc1c4
Crowdbotics <engineering@crowdbotics.com>
2023-04-18 10:10:28 +0000

    Update store.

--- store/newRavindertester/api.js
+++ store/newRavindertester/api.js
@@ -8,7 +8,13 @@ const newRavindertester = axios.create({
     Authorization: `Bearer ${NEW_RAVINDER_TESTER_TOKEN}`
   }
 })
+function newravindertester_get__read(payload) {
+  return newRavindertester.get(`/`)
+}
 function newravindertester_post_testercom_create(payload) {
   return newRavindertester.post(`/tester.com`)
 }
-export const apiService = { newravindertester_post_testercom_create }
+export const apiService = {
+  newravindertester_get__read,
+  newravindertester_post_testercom_create
+}
