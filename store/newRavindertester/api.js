2228c575739d7dc3cea24321223530f2b34c2932
Crowdbotics <engineering@crowdbotics.com>
2023-04-18 10:19:53 +0000

    Update store.

--- store/newRavindertester/api.js
+++ store/newRavindertester/api.js
@@ -1,5 +1,5 @@
-import axios from "axios";
-import { NEW_RAVINDER_TESTER_TOKEN } from "react-native-dotenv";
+import axios from "axios"
+import { NEW_RAVINDER_TESTER_TOKEN } from "react-native-dotenv"
 const newRavindertester = axios.create({
   baseURL: "https://youtube.com",
   headers: {
@@ -7,17 +7,14 @@ const newRavindertester = axios.create({
     "Content-Type": "application/json",
     Authorization: `Bearer ${NEW_RAVINDER_TESTER_TOKEN}`
   }
-});
-
+})
 function newravindertester_get__read(payload) {
-  return newRavindertester.get(`/`);
+  return newRavindertester.get(`/`)
 }
-
 function newravindertester_post_testercom_create(payload) {
-  return newRavindertester.post(`/tester.com`);
+  return newRavindertester.post(`/tester.com`)
 }
-
 export const apiService = {
   newravindertester_get__read,
   newravindertester_post_testercom_create
-};
+}
