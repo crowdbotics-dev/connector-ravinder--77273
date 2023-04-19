68c54dbebf2fa7e5d2f878fc0f40aca00f1ce24a
Ravinder sales <ravinder.y@crowdbotics.com>
2023-04-19 13:16:00 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled2/index.js
+++ screens/Untitled2/index.js
@@ -1,4 +1,5 @@
-import React from "react";
+import { Pressable } from "react-native";
+import { View } from "react-native";
 import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
 
 const Untitled2 = () => {
@@ -8,13 +9,26 @@ const Untitled2 = () => {
       padding: 10,
       position: 'relative',
       flex: 1
-    }}></ScrollView>
+    }}><Pressable><View style={styles.DFDHgEpn}></View></Pressable><View style={styles.zqwmbSRo}></View></ScrollView>
     </SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
   safeArea: {
     height: '100%'
+  },
+  DFDHgEpn: {
+    height: 60,
+    width: 140,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 0,
+    color: "#777777"
+  },
+  zqwmbSRo: {
+    width: 80,
+    height: 80,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 40
   }
 });
 export default Untitled2;
--- store/samNew/api.js
+++ store/samNew/api.js
@@ -1,11 +1,20 @@
-import axios from "axios"
-import { SAMNEW_SECRET } from "react-native-dotenv"
+import axios from "axios";
+import { SAMNEW_SECRET } from "react-native-dotenv";
 const samNew = axios.create({
   baseURL: "https://youtubve.com",
-  params: { private_key: SAMNEW_SECRET },
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
+  params: {
+    private_key: SAMNEW_SECRET
+  },
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
+});
+
 function samnew_get_seleniumcomm_read(payload) {
-  return samNew.get(`/selenium.comm`)
+  return samNew.get(`/selenium.comm`);
 }
-export const apiService = { samnew_get_seleniumcomm_read }
+
+export const apiService = {
+  samnew_get_seleniumcomm_read
+};
